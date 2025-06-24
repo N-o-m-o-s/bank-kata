53801afc2b0cd9cc8d9cdfde9ee0fcba93e5b4e6
nomos <maksimabcd1978@gmail.com>
2025-06-24 15:46:56 +0200

    feature/bank-account-logic - update BankAccount for correct use of TransactionFactory

--- src/main/java/org/example/bank/domain/BankAccount.java
+++ src/main/java/org/example/bank/domain/BankAccount.java
@@ -1,24 +1,37 @@
 package org.example.bank.domain;
 
+import org.example.bank.factory.TransactionFactory;
 import org.example.bank.util.AmountValidator;
 
+import java.time.Clock;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BankAccount {
     private final List<Transaction> transactions = new ArrayList<>();
     private int balance = 0;
+    private final TransactionFactory txFactory;
+
+    // Tests - inherit clock
+    public BankAccount(Clock clock) {
+        txFactory = new TransactionFactory(clock);
+    }
+
+    // Production - system clock
+    public BankAccount() {
+        this(Clock.systemDefaultZone());
+    }
 
     public void deposit(int amount) {
         AmountValidator.requirePositive(amount);
         balance += amount;
-        transactions.add(Transaction.ofDeposit(amount, balance));
+        transactions.add(txFactory.deposit(amount, balance));
     }
 
     public void withdraw(int amount) {
         AmountValidator.requirePositive(amount);
         balance -= amount;
-        transactions.add(Transaction.ofWithdrawal(amount, balance));
+        transactions.add(txFactory.withdrawal(amount, balance));
     }
 
     public List<Transaction> getTransactions() {
