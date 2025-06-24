53cf1a8ad416ba5732de0800ae42cb39eddbc0a7
nomos <maksimabcd1978@gmail.com>
2025-06-24 17:26:08 +0200

    feature/bank-account-logic - populate Main class with example from readme

--- src/main/java/org/example/bank/Main.java
+++ src/main/java/org/example/bank/Main.java
@@ -1,7 +1,20 @@
 package org.example.bank;
 
+import org.example.bank.domain.BankAccount;
+import org.example.bank.service.ConsoleStatementPrinter;
+
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        BankAccount account = new BankAccount(); // uses system clock
+
+        account.deposit(new BigDecimal("100.00"));
+        account.withdraw(new BigDecimal("30.50"));
+
+        String statement = new ConsoleStatementPrinter()
+                .print(account.getTransactions());
+
+        System.out.println(statement);
     }
 }
